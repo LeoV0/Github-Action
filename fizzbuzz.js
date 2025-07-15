ffec2019b26104bd3ad213adf145affc5b41cc1c
leoV0 <leo.cigale@gmail.com>
2025-07-15 10:51:25 +0200

    Fix fizzbuzz.js to get Fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,12 @@
 const fizzbuzz = (number) => {
-  if (number % 3 == 0) {
-    return "Fizz"
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "Fizzbuzz";
+  } else if (number % 3 == 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
   }
-  if (number === 5) {
-    return "Buzz"
-  }
-  return number
-}
+  return number;
+};
 
-module.exports = { fizzbuzz }
+module.exports = { fizzbuzz };
